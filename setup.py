8353e18bfb728370dc770fb1c5e7f95fc364372c
Peter Cotton <petercotton@Peters-MacBook-Pro.local>
2024-09-21 17:54:09 -0400

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="endersgame",
-    version="0.0.1",
+    version="0.0.2",
     description="Online autonomous time series prediction of near martingales",
     long_description=README,
     long_description_content_type="text/markdown",

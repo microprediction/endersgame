10fb5021d1015c21509d1eaa39bf5dfe5db8a038
Peter Cotton <petercotton@Peters-MacBook-Pro.local>
2024-09-21 10:56:34 -0400

    attacker example

--- endersgame/attackers/momentumattacker.py
+++ endersgame/attackers/momentumattacker.py
@@ -1,10 +1,12 @@
 from endersgame.attackers.baseattacker import BaseAttacker
+import numpy as np
 
 
 class MomentumAttacker(BaseAttacker):
 
     # This will receive one value at a time
     # If it thinks that the values will be higher than the current one in the future, it returns a positive number
+    # Here "future" means 100
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -12,8 +14,26 @@ class MomentumAttacker(BaseAttacker):
 
     def __call__(self,x:float)->float:
         self._buffer.append(x)
-        if len(self._buffer)<10:
-            
+        if len(self._buffer)<100:
+            return 0
+        else:
+            recent = self._buffer[-50:]
+            prior  = self._buffer[-100:-50]
+            diff = (np.mean(recent)-np.mean(prior))/(np.std(recent)+np.std(prior)+1e-6)
+            direction = int(diff)
+            return direction
+
+
+if __name__=='__main__':
+    xs = np.cumsum(np.random.randn(500))
+    attacker = MomentumAttacker()
+    for x in xs:
+        decision = attacker(x)
+        print(decision)
+
+
+
+
 
 
 

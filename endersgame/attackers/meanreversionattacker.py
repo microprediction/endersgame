c12944a5e640a86e6e310a96ccf94373fc398d8e
Peter Cotton <petercotton@Peters-MacBook-Pro.local>
2024-09-25 16:50:12 -0400

    silence

---
+++ endersgame/attackers/meanreversionattacker.py
@@ -0,0 +1,48 @@
+from endersgame.attackers.attackerwithsimplepnl import AttackerWithSimplePnL
+import numpy as np
+import types
+
+
+class MeanReversionAttacker(AttackerWithSimplePnL):
+
+    def __init__(self, a=0.01):
+        super().__init__()
+        self.state = {'running_avg': None,
+                      'current_value': None}
+        self.params = {'a': a}
+
+    def tick(self, x: float):
+        # Maintains an expon moving average of the data
+        self.state['current_value'] = x
+        if not np.isnan(x):
+            if self.state['running_avg'] is None:
+                self.state['running_avg'] = x
+            else:
+                self.state['running_avg'] = (1 - self.params['a']) * self.state['running_avg'] + self.params['a'] * x
+
+
+
+def predict(self, k:int=None)->float:
+    if self.state['current_value'] > self.state['running_avg'] + 1:
+        return -1
+    if self.state['current_value'] < self.state['running_avg'] - 1:
+        return 1
+    return 0
+
+
+
+
+if __name__=='__main__':
+
+    attacker = MeanReversionAttacker()  # Reset it
+    attacker.predict = types.MethodType(predict, attacker)
+
+    xs = [1, 3, 4, 2, 4, 5, 1, 5, 2, 5, 10] * 100
+    for x in xs:
+        y = attacker.tick_and_predict(x=x)
+        if abs(y):
+            print(y)
+    xs = [1, 3, 4, 2, 4, 5, 1, 5, 2, 5, 1] * 100
+    attacker = MeanReversionAttacker()
+    for x in xs:
+        y = attacker.tick_and_predict(x=54)

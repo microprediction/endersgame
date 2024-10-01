5cc03909741ec7e068bdbe418c5031d996ba94ea
Peter Cotton <petercotton@Peters-MacBook-Pro.local>
2024-10-01 13:33:45 -0400

    pnl

--- endersgame/examples/empiricalmacdattacker.py
+++
@@ -1,8 +0,0 @@
-from endersgame.examples.macdattacker import MacdAttacker
-from endersgame.attackers.calibratedattacker import CalibratedAttacker
-
-
-if __name__=='__main__':
-    mac = MacdAttacker()
-    emac = CalibratedAttacker(attacker=mac)
-

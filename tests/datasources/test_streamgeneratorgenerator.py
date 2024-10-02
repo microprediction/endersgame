fc21440aaaa6fb2ab3282964a32bb83a2e9e2c58
Peter Cotton <petercotton@Peters-MacBook-Pro.local>
2024-10-02 08:39:44 -0400

    ver

---
+++ tests/datasources/test_streamgeneratorgenerator.py
@@ -0,0 +1,14 @@
+
+# https://github.com/microprediction/endersnotebooks/blob/main/enders_data_generator.ipynb
+
+
+def test_gen_gen():
+    from endersgame.datasources.streamgeneratorgenerator import stream_generator_generator
+    gen_gen = stream_generator_generator(category='train')
+    for gen in gen_gen:
+        value = next(gen)
+        assert value is not None
+
+
+if __name__=='__main__':
+    test_gen_gen()

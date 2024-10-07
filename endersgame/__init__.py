cf69ceb138fcc816dbbd38f843981321b77b7c4e
Peter Cotton <petercotton@Peters-MacBook-Pro.local>
2024-10-07 14:30:56 -0400

    constants

--- endersgame/__init__.py
+++ endersgame/__init__.py
@@ -1,4 +1,6 @@
 from endersgame.gameconfig import EPSILON, HORIZON
+from endersgame.accounting.pnl import DEFAULT_TRADE_BACKOFF
+from endersgame.mixins.historymixin import DEFAULT_HISTORY_LEN
 from endersgame.attackers.attacker import Attacker
 from endersgame.datasources.streamgenerator import stream_generator
 from endersgame.datasources.streamgeneratorgenerator import stream_generator_generator

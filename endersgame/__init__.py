cda7d4b782bd76aae032d5dd9ef41440bd8f5967
Peter Cotton <petercotton@Peters-MacBook-Pro.local>
2024-10-07 16:32:22 -0400

    momentum

--- endersgame/__init__.py
+++ endersgame/__init__.py
@@ -4,3 +4,5 @@ from endersgame.mixins.historymixin import DEFAULT_HISTORY_LEN
 from endersgame.attackers.attacker import Attacker
 from endersgame.datasources.streamgenerator import stream_generator
 from endersgame.datasources.streamgeneratorgenerator import stream_generator_generator
+from endersgame.riverstats.fewmean import FEWMean
+from endersgame.riverstats.fewvar import FEWVar
--- tests/colabexamples/debug_momentum_attacker.py
+++ tests/colabexamples/debug_momentum_attacker.py
@@ -2,31 +2,45 @@
 # https://github.com/microprediction/endersnotebooks/blob/main/momentum_attacker.ipynb
 
 from endersgame import Attacker
-from endersgame import stream_generator, stream_generator_generator
-import numpy as np
-import types
+from endersgame import stream_generator_generator
 from pprint import pprint
-import json
-import scipy.optimize as opt
-from typing import Iterable, Iterator
-from pprint import pprint
-
+from endersgame import FEWMean, FEWVar
+import math
 
 class MyAttacker(Attacker):
 
-     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+     def __init__(self, fast_fading_factor:dict=0.1, slow_fading_factor=0.01, diff_fading_factor=0.001, threshold = 2, burn_in=100, **kwargs):
+         super().__init__(**kwargs)
+         self.threshold = threshold
+         self.fast_ewa = FEWMean(fading_factor=fast_fading_factor)
+         self.slow_ewa = FEWMean(fading_factor=slow_fading_factor)
+         self.diff_var = FEWVar(fading_factor=diff_fading_factor)    # Tracks mean and var of the difference
+         self.countdown = burn_in
 
      def tick(self, x:float):
-        pass
+         self.fast_ewa.tick(x=x)
+         self.slow_ewa.tick(x=x)
+         fast_minus_slow = self.fast_ewa.get() - self.slow_ewa.get()
+         self.diff_var.tick(x=fast_minus_slow)
+         self.countdown -= 1
 
      def predict(self, horizon: int = None) -> float:
-        return 0
-
+         """
+               We buy if signal > threshold*(trailing std of signal)
+         """
+         if self.countdown>0:
+             return 0    # Not warmed up
+         fast_minus_slow = self.fast_ewa.get() - self.slow_ewa.get()
+         try:
+             fast_minus_slow_std = math.sqrt( self.diff_var.get())
+             signal = int(fast_minus_slow/(self.threshold*fast_minus_slow_std))
+             return signal
+         except ArithmeticError:
+             return 0
 
 if __name__=='__main__':
     from endersgame.accounting.pnlutil import zero_pnl_summary, add_pnl_summaries
-    gen_gen = stream_generator_generator(category='train')
+    gen_gen = stream_generator_generator(category='test')
     attacker = MyAttacker()
     total_pnl = zero_pnl_summary()
     for stream in gen_gen:

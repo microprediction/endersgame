d3dfd34dff20e7ad7190ffaebea79c3dd41ce660
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-10-13 13:45:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="endersgame",
-    version="0.5.0",
+    version="1.0.0",
     description="Online autonomous time series prediction of near martingales",
     long_description=README,
     long_description_content_type="text/markdown",
